a40f3ec2c3c309a89eb665f4e073504ece3d2d2a
totally_lost <totally_lost@6d297dad-710a-0410-970a-977552b6d13a>
2006-03-01 00:35:22 +0000

    added parity and 64 bit r/w data path

--- trunk/fpgac/examples/pci/pci.c
+++ trunk/fpgac/examples/pci/pci.c
@@ -42,7 +42,9 @@ struct PCI_Config pci_config;
  * PCI State Machine Process
  */
 fpgac_process pci() {
-    int target_sm, next_target_sm;
+    char target_sm, next_target_sm;
+    char assert_par;
+    char assert_par64;
 
     next_target_sm = PCI_SM_Target_Idle;	// Default next state is idle
 
@@ -92,6 +94,8 @@ fpgac_process pci() {
         pci_config.Bar3 = 0;
         pci_config.Bar4 = 0;
         pci_config.Bar5 = 0;
+        assert_par = 0;
+        assert_par64 = 0;
 
     } else {
         pci_state.frame  = ~pci_bus.frame_;	// Make active high copies (get optimized away)
@@ -99,10 +103,51 @@ fpgac_process pci() {
         pci_state.irdy   = ~pci_bus.irdy_;
         pci_state.stop   = ~pci_bus.stop_;
         pci_state.devsel = ~pci_bus.devsel_;
-        pci_state.idsel  = ~pci_bus.idsel_;
         pci_state.gnt    = ~pci_bus.gnt_;
         pci_state.req64  = ~pci_bus.req64_;
         pci_state.ack64  = ~pci_bus.ack64_;
+
+        if(assert_par) {
+            pci_bus.par   = ( pci_bus.ad_b0    &1) ^ ((pci_bus.ad_b0>>1)&1)
+                          ^ ((pci_bus.ad_b0>>2)&1) ^ ((pci_bus.ad_b0>>3)&1)
+                          ^ ((pci_bus.ad_b0>>4)&1) ^ ((pci_bus.ad_b0>>5)&1)
+                          ^ ((pci_bus.ad_b0>>6)&1) ^ ((pci_bus.ad_b0>>7)&1)
+                          ^ ( pci_bus.ad_b1    &1) ^ ((pci_bus.ad_b1>>1)&1)
+                          ^ ((pci_bus.ad_b1>>2)&1) ^ ((pci_bus.ad_b1>>3)&1)
+                          ^ ((pci_bus.ad_b1>>4)&1) ^ ((pci_bus.ad_b1>>5)&1)
+                          ^ ((pci_bus.ad_b1>>6)&1) ^ ((pci_bus.ad_b1>>7)&1)
+                          ^ ( pci_bus.ad_b2    &1) ^ ((pci_bus.ad_b2>>1)&1)
+                          ^ ((pci_bus.ad_b2>>2)&1) ^ ((pci_bus.ad_b2>>3)&1)
+                          ^ ((pci_bus.ad_b2>>4)&1) ^ ((pci_bus.ad_b2>>5)&1)
+                          ^ ((pci_bus.ad_b2>>6)&1) ^ ((pci_bus.ad_b2>>7)&1)
+                          ^ ( pci_bus.ad_b3    &1) ^ ((pci_bus.ad_b3>>1)&1)
+                          ^ ((pci_bus.ad_b3>>2)&1) ^ ((pci_bus.ad_b3>>3)&1)
+                          ^ ((pci_bus.ad_b3>>4)&1) ^ ((pci_bus.ad_b3>>5)&1)
+                          ^ ((pci_bus.ad_b3>>6)&1) ^ ((pci_bus.ad_b3>>7)&1);
+        } else {
+#pragma fpgac_bus_idle(pci_bus.par)
+        }
+
+        if(assert_par64) {
+            pci_bus.par64 = ( pci_bus.ad_b4    &1) ^ ((pci_bus.ad_b4>>1)&1)
+                          ^ ((pci_bus.ad_b4>>2)&1) ^ ((pci_bus.ad_b4>>3)&1)
+                          ^ ((pci_bus.ad_b4>>4)&1) ^ ((pci_bus.ad_b4>>5)&1)
+                          ^ ((pci_bus.ad_b4>>6)&1) ^ ((pci_bus.ad_b4>>7)&1)
+                          ^ ( pci_bus.ad_b5    &1) ^ ((pci_bus.ad_b5>>1)&1)
+                          ^ ((pci_bus.ad_b5>>2)&1) ^ ((pci_bus.ad_b5>>3)&1)
+                          ^ ((pci_bus.ad_b5>>4)&1) ^ ((pci_bus.ad_b5>>5)&1)
+                          ^ ((pci_bus.ad_b5>>6)&1) ^ ((pci_bus.ad_b5>>7)&1)
+                          ^ ( pci_bus.ad_b6    &1) ^ ((pci_bus.ad_b6>>1)&1)
+                          ^ ((pci_bus.ad_b6>>2)&1) ^ ((pci_bus.ad_b6>>3)&1)
+                          ^ ((pci_bus.ad_b6>>4)&1) ^ ((pci_bus.ad_b6>>5)&1)
+                          ^ ((pci_bus.ad_b6>>6)&1) ^ ((pci_bus.ad_b6>>7)&1)
+                          ^ ( pci_bus.ad_b7    &1) ^ ((pci_bus.ad_b7>>1)&1)
+                          ^ ((pci_bus.ad_b7>>2)&1) ^ ((pci_bus.ad_b7>>3)&1)
+                          ^ ((pci_bus.ad_b7>>4)&1) ^ ((pci_bus.ad_b7>>5)&1)
+                          ^ ((pci_bus.ad_b7>>6)&1) ^ ((pci_bus.ad_b7>>7)&1);
+        } else {
+#pragma fpgac_bus_idle(pci_bus.par64_)
+        }
     }
 
     if(target_sm & PCI_SM_Target_Idle) {
@@ -118,27 +163,38 @@ fpgac_process pci() {
 #pragma fpgac_bus_idle(pci_bus.trdy_)
 #pragma fpgac_bus_idle(pci_bus.stop_)
 #pragma fpgac_bus_idle(pci_bus.devsel_)
-#pragma fpgac_bus_idle(pci_bus.par_)
-#pragma fpgac_bus_idle(pci_bus.par64_)
 #pragma fpgac_bus_idle(pci_bus.perr_)
 
+        assert_par = 0;                         // Will release par after next clock
+        assert_par64 = 0;                       // Will release par64 after next clock
+
         if(pci_state.frame & !pci_state.lastframe) {
-            pci_state.cmd   = ~pci_bus.cbe_;	// Latch cmd and address when frame asserted
-            pci_state.ad_b0 = pci_bus.ad_b0;
-            pci_state.ad_b1 = pci_bus.ad_b1;
-            pci_state.ad_b2 = pci_bus.ad_b2;
-            pci_state.ad_b3 = pci_bus.ad_b3;
-            pci_state.rw = pci_state.cmd & 1;
+            pci_state.cmd     = ~pci_bus.cbe_;	// Latch cmd and address when frame asserted
+            pci_state.rw      = (~pci_bus.cbe_) & 1;
+            pci_state.addr_b0 = pci_bus.ad_b0;
+            pci_state.addr_b1 = pci_bus.ad_b1;
+            pci_state.addr_b2 = pci_bus.ad_b2;
+            pci_state.addr_b3 = pci_bus.ad_b3;
+            if(pci_state.req64) {
+                pci_state.addr_b4 = pci_bus.ad_b4;
+                pci_state.addr_b5 = pci_bus.ad_b5;
+                pci_state.addr_b6 = pci_bus.ad_b6;
+                pci_state.addr_b7 = pci_bus.ad_b7;
+            }
 
             if(pci_state.cmd & PCI_CMD_Memory) { // Memory RW, so check if our addresses
                 pci_state.bar0 = pci_bus.ad_b3 == ((pci_config.Bar0>>23) & 0xff);
                 pci_state.bar1 = pci_bus.ad_b3 == ((pci_config.Bar1>>23) & 0xff);
-                if(pci_state.bar0 | pci_state.bar1)
+                if(pci_state.bar0 | pci_state.bar1) {
                     next_target_sm = PCI_SM_Target_B_Busy;
+                    pci_bus.devsel_ = 0;         // Now driving, must be tristated at end of cycle
+                    if(pci_state.req64)
+                        pci_bus.ack64_ = 0;      // Now driving, must be tristated at end of cycle
+                }
 
-            } else {				// IO or Config space access
+            } else {                             // IO or Config space access
                 pci_state.bar0 = pci_state.bar1 = 0;
-                if(idsel && ((pci_state.cmd & 0xfe) == PCI_CMD_Config_Read))  // Config RW
+                if(pci_bus.idsel && ((pci_state.cmd & 0xfe) == PCI_CMD_Config_Read))  // Config RW
                     next_target_sm = PCI_SM_Target_Config;
             }
         } else ;// next_target_sm = PCI_SM_Target_Idle;
@@ -155,11 +211,41 @@ fpgac_process pci() {
     }
 
     if(target_sm & PCI_SM_Target_S_Data) {
+        unsigned long data;
 
-        /*
-         * Place read/write interfaces to backend here
-         */
-
+        if(pci_state.rw) {			// Memory Space Write
+            if(pci_state.bar0) {
+                /*
+                 * Place write interfaces to bar0 backend here
+                 */
+            }
+            if(pci_state.bar1) {
+                /*
+                 * Place write interfaces to bar1 backend here
+                 */
+            }
+        } else {				// Memory Space Read
+            if(pci_state.bar0) {
+                /*
+                 * Place read interfaces to bar0 backend here
+                 */
+            }
+            if(pci_state.bar1) {
+                /*
+                 * Place read interfaces to bar1 backend here
+                 */
+            }
+            if(~pci_bus.cbe_ & 0x01) pci_bus.ad_b0 =  data      & 0xff;
+            if(~pci_bus.cbe_ & 0x02) pci_bus.ad_b1 = (data>> 8) & 0xff;
+            if(~pci_bus.cbe_ & 0x04) pci_bus.ad_b2 = (data>>16) & 0xff;
+            if(~pci_bus.cbe_ & 0x08) pci_bus.ad_b3 = (data>>24) & 0xff;
+            if(~pci_bus.cbe_ & 0x10) pci_bus.ad_b4 = (data>>32) & 0xff;
+            if(~pci_bus.cbe_ & 0x20) pci_bus.ad_b5 = (data>>40) & 0xff;
+            if(~pci_bus.cbe_ & 0x40) pci_bus.ad_b6 = (data>>48) & 0xff;
+            if(~pci_bus.cbe_ & 0x80) pci_bus.ad_b7 = (data>>56) & 0xff;
+            assert_par = 1;                     // assert par after next clock
+            assert_par64 = 1;                   // assert par64 after next clock
+        }
         if(!(pci_state.irdy)) next_target_sm = PCI_SM_Target_S_Data;
         else if(pci_state.irdy & pci_state.frame) next_target_sm = PCI_SM_Target_BackOff;
         else ;// next_target_sm = PCI_SM_Target_Idle;
@@ -174,7 +260,7 @@ fpgac_process pci() {
         unsigned char config_addr;
         unsigned long data;
 
-        config_addr = pci_state.ad_b0 & 0xff;
+        config_addr = pci_state.addr_b0 & 0xff;
         if(pci_state.rw) {			// Configuration Space Write
             if(config_addr == PCI_CONFIG_status_cmd)
                 pci_config.Command = ((pci_bus.ad_b1<<8) | pci_bus.ad_b0) & 0x2ff;
@@ -198,10 +284,11 @@ fpgac_process pci() {
                 data = ((pci_config.SubSystem_ID & 0xffff) << 16) | (pci_config.SubVendor_ID & 0xffff);
 
             // Now driving data bus, must be tristated at end of cycle
-            if(pci_bus.cbe & 0x01) pci_bus.ad_0 =  data      & 0xff;
-            if(pci_bus.cbe & 0x02) pci_bus.ad_1 = (data>>8)  & 0xff;
-            if(pci_bus.cbe & 0x04) pci_bus.ad_2 = (data>>16) & 0xff;
-            if(pci_bus.cbe & 0x08) pci_bus.ad_3 = (data>>16) & 0xff;
+            if(~pci_bus.cbe_ & 0x01) pci_bus.ad_b0 =  data      & 0xff;
+            if(~pci_bus.cbe_ & 0x02) pci_bus.ad_b1 = (data>>8)  & 0xff;
+            if(~pci_bus.cbe_ & 0x04) pci_bus.ad_b2 = (data>>16) & 0xff;
+            if(~pci_bus.cbe_ & 0x08) pci_bus.ad_b3 = (data>>24) & 0xff;
+            assert_par = 1;
         }
         pci_bus.trdy_ = pci_bus.devsel_ = 0; // Now driving, must be tristated at end of cycle
 
