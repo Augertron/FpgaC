2aaef7d33858a5f52d1df759370e35c5104cfc0f
totally_lost <totally_lost@6d297dad-710a-0410-970a-977552b6d13a>
2006-04-10 19:10:26 +0000

    added enum support

---
+++ trunk/fpgac/examples/regression_tests/enum_2.c
@@ -0,0 +1,11 @@
+fpgac_output out:3;
+
+enum abc {a=2, b, c};
+
+main()
+{
+	out = b;
+}
+
+
+
---
+++ trunk/fpgac/examples/regression_tests/enum_4.c
@@ -0,0 +1,14 @@
+fpgac_output out:3;
+
+enum abc {a, b, c};
+enum abc eval;
+
+main()
+{
+	eval = b;
+	eval += 1;
+	out = eval;
+}
+
+
+
